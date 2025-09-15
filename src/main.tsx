d4b061346d02df1c2fc32baaca8f943c783ce23d
Rookro <rookro.public@gmail.com>
2025-09-15 15:11:11 +0000

    feat: アプリ名を設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./Store";
+import { Provider, useDispatch } from "react-redux";
+import { listen } from "@tauri-apps/api/event";
+import { AppDispatch, store } from "./Store";
+import { getEntriesInZip, setContainerPath } from "./reducers/FileReducer";
 import App from "./App";
 
 // デフォルト動作では右クリックでメニューが開くため、開かないように抑制する
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+// ドラッグアンドドロップでファイルを指定する
+// 複数指定された場合は、最初の一つのみ
+listen("tauri://drag-drop", (event) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const path = (event.payload as { paths: string[] }).paths[0];
+  dispatch(setContainerPath(path));
+  dispatch(getEntriesInZip(path));
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
