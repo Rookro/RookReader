b44e9b11a32bb0f7d85a95e1a1e513fbf6b62008
Rookro <rookro.public@gmail.com>
2025-11-20 23:28:45 +0900

    feat: サイドバーの追加と画像一覧表示を追加
    * サイドバーのアイコンでファイルエクスプローラーとコンテナ内の画像一覧を切り替えられるように

---
+++ src/components/TabPanel/TabPanel.tsx
@@ -0,0 +1,35 @@
+import { Box } from "@mui/material";
+
+/**
+ * タブパネルコンポーネントのプロパティ
+ */
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+/**
+ * タブパネルコンポーネント
+ */
+const TabPanel = (props: TabPanelProps) => {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ width: '100%', height: '100%' }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
+}
+
+export default TabPanel;
