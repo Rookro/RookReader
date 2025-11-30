927954d291fa28ddff2d201bb66e8deac23ab89b
Rookro <rookro.public@gmail.com>
2025-11-30 13:37:54 +0900

    feat: 言語変更を別ウィンドウにも即時反映する
    * i18n の changeLanguage() は WebView 毎に独立しているので、設定画面で言語変更されたらイベントを送ってメイン画面でも changeLanguage() を実行する

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useRef } from "react";
 import { CssBaseline, Stack, ThemeProvider } from "@mui/material";
 import { Panel, PanelGroup, PanelResizeHandle, } from "react-resizable-panels";
+import { listen, UnlistenFn } from "@tauri-apps/api/event";
+import { debug } from "@tauri-apps/plugin-log";
 import ControlSlider from "./components/ControlSlider/ControlSlider";
 import ImageViewer from "./components/ImageViewer/ImageViewer";
 import LeftPane from "./components/LeftPane/LeftPane";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import { useAppTheme } from "./hooks/useAppTheme";
+import i18n from "./i18n/config";
 
 export default function App() {
   const theme = useAppTheme();
+  const unlistenRef = useRef<UnlistenFn>(null);
+
+  useEffect(() => {
+    const listenLanguageChangedEvent = async () => {
+      const unlisten = await listen<{ language: string }>('i18n-language-changed', (event) => {
+        debug(`Received language changed event: ${event.payload.language}`);
+        i18n.changeLanguage(event.payload.language);
+      })
+      unlistenRef.current?.();
+      unlistenRef.current = unlisten;
+    };
+    listenLanguageChangedEvent();
+
+    return () => {
+      unlistenRef.current?.();
+    }
+  }, [])
 
   return (
     <ThemeProvider theme={theme}>
--- src/components/Settings/GeneralSettings/Items/LanguageSettings.tsx
+++ src/components/Settings/GeneralSettings/Items/LanguageSettings.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { emit } from "@tauri-apps/api/event";
+import { debug } from "@tauri-apps/plugin-log";
 import { Box, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
-import { settingsStore } from "../../../../settings/SettingsStore";
 
 /**
  * Language setting component.
@@ -12,8 +13,12 @@ export default function LanguageSetting() {
 
     const handleLanguageChanged = async (e: SelectChangeEvent) => {
         setLanguage(e.target.value);
-        settingsStore.set("language", e.target.value);
-        i18n.changeLanguage(e.target.value === "default" ? undefined : e.target.value);
+        i18n.changeLanguage(e.target.value).then(() => {
+            // Emit an event to call changeLanguage() in the other WebView contexts as well,
+            // since the language change is immediately reflected only within its own WebView context.
+            emit('i18n-language-changed', { language: e.target.value });
+            debug(`Language changed: ${e.target.value}`);
+        });;
     }
 
     return (
