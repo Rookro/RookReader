7bd8eb288fe9c1716e898f84251ffb076aae908a
Rookro <rookro.public@gmail.com>
2025-11-30 22:10:48 +0900

    build: ファイル選択ダイアログのプラグインを追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,7 @@ mod state;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let result = tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .manage(Mutex::new(state::app_state::AppState::default()))
         .plugin(tauri_plugin_os::init())
