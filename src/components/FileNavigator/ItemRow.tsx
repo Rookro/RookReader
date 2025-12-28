e69f2ac5f888920019257c0168af90f16cec7cde
Rookro <rookro.public@gmail.com>
2025-12-28 14:22:43 +0900

    refactor: FileNavigatorを整理
    * Tauri の watch をカスタムフックにする
    * ファイル選択のロジックをカスタムフックにする
    * Rowコンポーネントを別ファイルに分離
    * AND検索やソートの関数をUtilsに分離

---
+++ src/components/FileNavigator/ItemRow.tsx
@@ -0,0 +1,39 @@
+import { CSSProperties, memo } from 'react';
+import { ListItem, ListItemButton, ListItemText, Tooltip } from '@mui/material';
+import { Folder, InsertDriveFile } from '@mui/icons-material';
+import { DirEntry } from '../../types/DirEntry';
+
+/**
+ * Row component for the file navigator.
+ */
+export const ItemRow = memo(function ItemRow({
+    entry,
+    index,
+    selected,
+    onClick,
+    onDoubleClick,
+    style
+}: {
+    entry: DirEntry;
+    index: number;
+    selected: boolean;
+    onClick: (e: React.MouseEvent<HTMLDivElement>, entry: DirEntry, index: number) => void;
+    onDoubleClick: (e: React.MouseEvent<HTMLDivElement>, entry: DirEntry) => void;
+    style: CSSProperties | undefined
+}) {
+    return (
+        <Tooltip title={entry.name} placement="right-start">
+            <ListItem style={style} key={index} component="div" disablePadding dense>
+                <ListItemButton
+                    selected={selected}
+                    onClick={(e) => onClick(e, entry, index)}
+                    onDoubleClick={(e) => onDoubleClick(e, entry)}
+                    key={entry.name}
+                >
+                    {entry.is_directory ? <Folder /> : <InsertDriveFile />}
+                    <ListItemText primary={entry.name} slotProps={{ primary: { noWrap: true } }} sx={{ marginLeft: "5px" }} />
+                </ListItemButton>
+            </ListItem>
+        </Tooltip>
+    );
+});
