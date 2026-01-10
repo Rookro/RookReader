68a60454975e0c2af0a1029230381c18bb10ee67
Rookro <rookro.public@gmail.com>
2026-01-11 02:08:51 +0900

    Aboutページのデザインの変更

--- src/components/Settings/AboutPage/AboutPage.tsx
+++ src/components/Settings/AboutPage/AboutPage.tsx
@@ -1,5 +1,4 @@
-import { useTranslation } from "react-i18next";
-import { Typography, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import About from "./Items/About";
 import ThirdParty from "./Items/ThirdParty";
 
@@ -7,11 +6,8 @@ import ThirdParty from "./Items/ThirdParty";
  * About page component.
  */
 export default function AboutPage() {
-    const { t } = useTranslation();
-
     return (
         <Stack spacing={3}>
-            <Typography variant="h5">{t('settings.about.title')}</Typography>
             <About />
             <ThirdParty />
         </Stack>
