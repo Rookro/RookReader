575efa43ed0b193c4e72c95a812700b6c6d3b5af
Rookro <rookro.public@gmail.com>
2026-01-11 01:02:13 +0900

    feat: 各設定画面のベースとなるパネルを追加

---
+++ src/components/Settings/SettingsPanel.tsx
@@ -0,0 +1,31 @@
+import { Container, Paper, SxProps, Typography } from "@mui/material";
+
+/**
+  * Properties for the TabPanel component.
+ */
+interface SettingsPanelProps {
+    title: string;
+    children?: React.ReactNode;
+    sx?: SxProps;
+}
+
+/**
+ * Tab panel component.
+ */
+export default function SettingsPanel(props: SettingsPanelProps) {
+    const { title, children, sx, ...other } = props;
+
+    return (
+        <Container
+            sx={sx}
+            {...other}
+        >
+            <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold' }}>
+                {title}
+            </Typography>
+            <Paper elevation={3} sx={{ padding: 2 }}>
+                {children}
+            </Paper>
+        </Container>
+    );
+};
