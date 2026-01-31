89500bdcbd450a2f92eeb31be5f051361585aa4c
Rookro <rookro.public@gmail.com>
2026-01-31 18:15:06 +0900

    refactor: debounce の実装を mui のdebounce を利用するように変更

--- src/middleware/historyMiddleware.ts
+++ src/middleware/historyMiddleware.ts
@@ -1,9 +1,22 @@
 import { Middleware } from "@reduxjs/toolkit";
 import { HistoryTable } from "../database/historyTable";
 import { error } from "@tauri-apps/plugin-log";
+import { debounce } from "@mui/material";
 
-let debounceTimerId: number | null = null;
 let historyTable: HistoryTable | null = null;
+const debouncedHistoryUpdate = debounce(
+  async (filePath: string, type: "DIRECTORY" | "FILE", index: number) => {
+    if (!historyTable) {
+      historyTable = new HistoryTable();
+    }
+    try {
+      await historyTable.upsert(filePath, type, index);
+    } catch (e) {
+      error(`History update failed (${filePath}:${index}): ${e}`);
+    }
+  },
+  500,
+);
 
 export const historyMiddleware: Middleware = (store) => (next) => async (action: unknown) => {
   const result = next(action);
@@ -19,24 +32,7 @@ export const historyMiddleware: Middleware = (store) => (next) => async (action:
       const { history, historyIndex, index, isDirectory } = state.file.containerFile;
 
       if (history[historyIndex] && index > -1) {
-        if (debounceTimerId) {
-          clearTimeout(debounceTimerId);
-        }
-        debounceTimerId = setTimeout(async () => {
-          if (!historyTable) {
-            historyTable = new HistoryTable();
-          }
-          try {
-            await historyTable.upsert(
-              history[historyIndex],
-              isDirectory ? "DIRECTORY" : "FILE",
-              index,
-            );
-          } catch (e) {
-            error(`History update failed (${history[historyIndex]}:${index}): ${e}`);
-          }
-          debounceTimerId = null;
-        }, 500);
+        debouncedHistoryUpdate(history[historyIndex], isDirectory ? "DIRECTORY" : "FILE", index);
       }
     }
   }
